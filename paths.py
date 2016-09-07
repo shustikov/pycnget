8a3d2662c2242300260ce7a9b51c558c0ca113e7
Shustikov Alexey <shustikov.alexey@gmail.com>
2016-09-07 21:48:21 +0300

    pathS fix

--- paths.py
+++ paths.py
@@ -4,40 +4,40 @@
 """
 
 import os
-dir_current = os.path.dirname(os.path.abspath(__file__)) + '/'
+# current app dir
+dс = os.path.dirname(os.path.abspath(__file__)) + '/'
 
-file_log =    'log/log.txt'			#Общий лог
-file_logok =  'log/logok.txt'       #Лог успешных попыток 
-file_lognok = 'log/lognok.txt'      #Лог неудачных попыток
-file_res =    'res/res.csv'         #Общий файл результата
-file_resh =   'res/cnresh.csv'      #
-file_ghp0 =   'src/ЖП.csv'          #
-file_ghp1 =   'src/csv ЖП.csv'      #
-file_ghp2 =   'src/csv ЖП2.csv'     #
-file_houses = 'src/houses.csv'      #
-file_resp =   'res/tmp/resp.csv'	#
-file_resph =  'res/tmp/resph.csv'   #
-file_mkd =    'src/MKD.csv'
-file_hcn =    'res/tmp/housecn.csv'  
+# dirs
+pathf_resap =   dc + 'res/temp/'
+pathf_resh  =   dc + 'res/tmp/houses/'
+pathf_resap =   dc + 'res/apartments/'
+pathf_logs	=   dc + 'log/'
 
+# files
+# sourses
+path_ghp0 =   dс + 'src/ЖП.csv'          #
+path_ghp1 =   dс + 'src/csv ЖП.csv'      #
+path_ghp2 =   dс + 'src/csv ЖП2.csv'     #
+path_houses = dс + 'src/houses.csv'      #
+path_resph =  dс + 'src/MKD.csv'
 
-path_resf =   dir_current + 'res/'
+# logs
+path_log =    dс + 'log/log.txt'		 # Общий лог
+path_logok =  dс + 'log/logok.txt'       # Лог успешных попыток 
+path_lognok = dс + 'log/lognok.txt'      # Лог неудачных попыток 
 
-path_log =    dir_current + file_log
-path_logok =  dir_current + file_logok 
-path_lognok = dir_current + file_lognok  
-path_res =    dir_current + file_res
-path_ghp0 =   dir_current + file_ghp0 
-path_ghp1 =   dir_current + file_ghp1
-path_ghp2 =   dir_current + file_ghp2
-path_houses = dir_current + file_houses
-path_resh =   dir_current + file_resh
-path_resp =   dir_current + file_resp
-path_resph =  dir_current + file_resph
-path_mkd = 	  dir_current + file_mkd
-path_hcn =    dir_current + file_hcn
+# temp results
+path_res =    dс + 'res/res.csv'         # Общий файл результата (для тестов)
+
+path_resh =   dс + 'res/tmp/resph.csv'	 # Файл ответов по домам
+path_cnh = 	  dс + 'res/tmp/cnh.csv' 	 # Файл содержащий дома и КНы
+
+path_resp =   dс + 'res/tmp/resp.csv'    # Файл ответов по квартирам   
+path_hcn =    dс + 'res/tmp/cn.csv'		 # файл квартиры и КНы	
 
 
 if __name__ == '__main__':
   print(path_log)
   input("Press enter to exit ;)")
+  
+  
